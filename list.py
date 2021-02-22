7201139c9c0e5bf7f2924ef68253fd5a340721e3
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-02-21 22:28:29 -0500

    list practice

---
+++ list.py
@@ -0,0 +1,11 @@
+our_list = [27, 46, -5, 17, 99]
+print(our_list)
+
+print(type(our_list))
+
+Jackson = ["a", "B", "C", 1, 2, 3, "Do", "Rey", "Mi", True, False]
+print(Jackson[7])
+print(Jackson[2:5:1])
+
+new_list = [1,2, [3,4,5], 6,7,8]
+print(new_list[2][1])
