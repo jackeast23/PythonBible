f775ee22576baf76dfa3185d01752a04ba47fb0a
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-02-22 19:54:06 -0500

    tuples practice

---
+++ tuples.py
@@ -0,0 +1,16 @@
+our_tuple = (1,2,3,"A", "B","C")
+
+print(type(our_tuple))
+
+
+print(our_tuple[0:3])
+
+our_list = [1,2,3,4,5,6,7]
+our_list[2] = 100
+print(our_list)
+
+# This will not work for tuples
+# our_tuple[2] = 100 
+
+A = [1,2,3]
+print(tuple(A))
